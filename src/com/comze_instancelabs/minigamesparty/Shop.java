9c3b467cb1fec5ca75adc9d8ff4e79419bcd5235
InstanceLabs <danielratke97@hotmail.de>
2014-02-18 16:16:26 +0100

    adds Shop class

---
+++ src/com/comze_instancelabs/minigamesparty/Shop.java
@@ -0,0 +1,28 @@
+package com.comze_instancelabs.minigamesparty;
+
+public class Shop {
+
+	//TODO shop
+	
+	// Shop will open an item window
+	
+	/*
+	 * ideas:
+	 * 
+	 * Spleef: grenades
+	 * 
+	 * Overall Style: colored Armor
+	 * 
+	 * ColorMatch: jump boost
+	 * 
+	 */
+	
+	
+	public static int getPlayerShopComponent(Main m, String p, String component){
+		return 0;
+	}
+	
+	public static void setPlayerShopComponent(Main m, String p, String component){
+		
+	}
+}
