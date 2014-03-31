2c1cdb2c44ad831d2a0a0cc63cce988021394e0e
InstanceLabs <danielratke97@hotmail.de>
2014-03-31 19:50:20 +0200

    important hotfix regarding mc versions

--- src/com/comze_instancelabs/minigamesparty/ParticleManager.java
+++ src/com/comze_instancelabs/minigamesparty/ParticleManager.java
@@ -6,14 +6,18 @@ import org.bukkit.Location;
 public class ParticleManager {
 	
 	public static void runManager(Location l, int i, int j){
-		if (Bukkit.getVersion().contains("1.6.4") || Bukkit.getVersion().contains("1.6.2")) {
+		if (Bukkit.getVersion().contains("MC: 1.6.4") || Bukkit.getVersion().contains("1.6.2")) {
 			ParticleEffect164 explosion = ParticleEffect164.HUGE_EXPLOSION;
 			explosion.animateAtLocation(l, i, j);
-		}else if(Bukkit.getVersion().contains("1.7.2")){
+		}else if(Bukkit.getVersion().contains("MC: 1.7.2")){
 			ParticleEffect172 explosion = ParticleEffect172.HUGE_EXPLOSION;
 			explosion.animateAtLocation(l, i, j);
-		}else if(Bukkit.getVersion().contains("1.7.5")){
+		}else if(Bukkit.getVersion().contains("MC: 1.7.5")){
 			ParticleEffect175 explosion = ParticleEffect175.HUGE_EXPLOSION;
+			explosion.animateAtLocation(l, i, j);
+		}else{
+			//fallback
+			ParticleEffect172 explosion = ParticleEffect172.HUGE_EXPLOSION;
 			explosion.animateAtLocation(l, i, j);
 		}
 	}
